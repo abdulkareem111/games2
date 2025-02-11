50b5fb2deaebd0a0a9ecab35081a6d7cd2b69350
Abdul Karim <abdulkarim@Abduls-MacBook-Air.local>
2025-02-11 16:25:40 +0400

    updating db and security

--- db.js
+++ db.js
@@ -1,13 +1,13 @@
-// db.js
+require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: process.env.DB_HOST || 'sql12.freesqldatabase.com',
-  user: process.env.DB_USER || 'sql12762180',
-  password: process.env.DB_PASSWORD || 'KFjjujU79f',
-  database: process.env.DB_NAME || 'sql12762180',
-  port: process.env.DB_PORT || 3306
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 // A helper function to execute queries.
--- utils/db.js
+++ utils/db.js
@@ -1,13 +1,14 @@
+require('dotenv').config();
 const mysql = require('mysql2');
 const util = require('util');
 
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: process.env.DB_HOST || 'sql12.freesqldatabase.com',
-    user: process.env.DB_USER || 'sql12762180',
-    password: process.env.DB_PASSWORD || 'KFjjujU79f',
-    database: process.env.DB_NAME || 'sql12762180',
-    port: process.env.DB_PORT || 3306
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
   });
 
 pool.query = util.promisify(pool.query);
